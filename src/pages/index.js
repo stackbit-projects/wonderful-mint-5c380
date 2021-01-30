6df5c202dc45be4534a47fee67dbd835e3db61ab
Wursthub <christophgoetz@gmail.com>
2021-01-30 20:40:49 +0000

    index.js: updated Header 1 in Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
     <div id="main">
       <header className="major container medium">
         <h2>
-          We conduct experiments that
+          testsetsetset
           <br />
           may or may not seriously
           <br />
